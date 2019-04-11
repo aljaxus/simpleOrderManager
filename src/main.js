5b07468e69c353ab37d285fae6fb4869e0337d53
aljaxus <me@aljaxus.eu>
2019-04-11 13:49:51 +0200

    Added i18n (multi-locale) plugin

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import store from "./store";
 import "./registerServiceWorker";
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import i18n from './i18n'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
