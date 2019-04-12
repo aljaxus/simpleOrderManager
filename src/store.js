dde3bc11f9ec3da2400b3166db6afa7937a0cae9
aljaxus <me@aljaxus.eu>
2019-04-12 14:23:26 +0200

    Set up VueX structure (sidebar saving etc)

--- src/store.js
+++ src/store.js
@@ -1,10 +1,58 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import vuexLocal from "./storepresist"
+
 
 Vue.use(Vuex);
 
+const App = {
+  namespaced: true,
+  state: {
+    sidebarShown: false,
+    sidebarMini: false,
+    darkMode: true,
+  },
+  mutations: {
+    TOGGLE_SIDEBAR_STATE (state, newState = null) { state.sidebarShown = newState!=null?newState:!state.sidebarShown },
+    TOGGLE_SIDEBAR_MINI (state, newState = null) { state.sidebarMini = newState!=null?newState:!state.sidebarMini },
+    TOGGLE_DARKMODE (state, newState = null) { state.darkMode = newState!=null?newState:!state.darkMode },
+  },
+  actions: {
+    toggleSidebarState (context, newState = null) { context.commit('TOGGLE_SIDEBAR_STATE', newState) },
+    toggleSidebarMini (context, newState = null) { context.commit('TOGGLE_SIDEBAR_MINI', newState) },
+    toggleDarkMode (context, newState = null) { context.commit('TOGGLE_DARKMODE', newState) },
+  },
+  getters: {
+    sidebarState: state => state.sidebarShown,
+    sidebarMini: state => state.sidebarMini,
+    darkMode: state => state.darkMode,
+  }
+}
+
+const Products = {
+  namespaced: true,
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+}
+
+
 export default new Vuex.Store({
+  modules: {
+    App,
+    Products
+  },
   state: {},
   mutations: {},
-  actions: {}
-});
+  actions: {},
+  plugins: [vuexLocal.plugin]
+})
