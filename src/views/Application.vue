8e1cf4f7cb5a9cc6fd84e25ad91383c3b8fa66ab
aljaxus <me@aljaxus.eu>
2019-04-14 14:05:14 +0200

    Improvements

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -1,35 +1,44 @@
 <template>
   <v-layout row wrap fill-height>
-    <v-flex xs6 class="pa-1" d-flex>
-      <v-card fill-height>
+    <v-flex xs6 class="pa-1">
+      <v-card
+        class="my-2"
+        v-for="i in 4"
+        :key="i"
+      >
         <v-card-text>
-          <v-card
-            class="my-2"
-            v-for="i in 4"
-            :key="i"
-          >
-            <v-card-text>
-              {{ i + '' }}  
-            </v-card-text>
-          </v-card>
+          {{ i + '' }}  
         </v-card-text>
-      </v-card>
+      </v-card>      
     </v-flex>
-    <v-flex xs6 class="pa-1" d-flex>
-      <v-card>
+    <v-flex xs6 class="pa-1">
+      <v-card
+        class="my-2"
+        v-for="(o, i) in orders"
+        :key="i"
+      >
         <v-card-text>
-          ORDERS
+          {{ i + '' }}  
         </v-card-text>
-      </v-card>
+      </v-card>      
     </v-flex>
   </v-layout>
 </template>
 <script>
+import Store from '@/store'
+const test = [
+  'testing'
+]
+
+
 export default {
   name: 'ApplicationRoute',
   data: () => { return {
+    Store
+  }},
+  computed: {
 
-  }}  
+  }
 }
 </script>
 <style>
